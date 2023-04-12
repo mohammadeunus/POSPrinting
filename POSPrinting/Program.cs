aee9a01e9173a743dfc6343f7a7ad15a297be6b0
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-04-12 06:22:57 +0600

    removed unnecessary method in program cs file

--- POSPrinting/Program.cs
+++ POSPrinting/Program.cs
@@ -2,35 +2,17 @@
 using System.IO.Ports;
 using System.Text;
 
-namespace POSPrinting
-{
-    class Program
-    {
-        // Method to print the lines of text to the console
-        static void PrintLines(int LineCount, List<string> linesOfFoodName, List<string> linesOfComment, List<string> linesOfDate)
-        {
-            for (int i = 0; i < LineCount; i++)
-            {
-                Console.WriteLine($"{linesOfDate[i]}  {linesOfFoodName[i]}  {linesOfComment[i]}");
-            }
-        }
+using POSPrinting;
+
+// Get input data from the user
+string foodName = "HamburgerSandwich";
+string columnsBody = "This hamburger sandwich was delicious! Perfectly cooked patty, fresh toppings, and a soft bun made for a great balance of flavors and textures.";
+string dateTime = DateTime.Now.ToString("hh:mm:ss tt");
+byte CharInEachLine = 42;
+byte spaceCountBetweenColumn = 1;
 
 
-        static void Main(string[] args)
-        {
-            // Get input data from the user
-            string foodName = "HamburgerSandwich";
-            string comment = "This hamburger sandwich was delicious! Perfectly cooked patty, fresh toppings, and a soft bun made for a great balance of flavors and textures.";
-            string dateTime = DateTime.Now.ToString("hh:mm:ss tt");
-            byte CharInEachLine = 42;
-            byte spaceCountBetweenColumn = 2;
-
-
-            // Create a new instance of the ReceiptPrinter class and ...
-            ReceiptPrinter ORestaurant = new ReceiptPrinter(foodName, comment, dateTime);
-            ORestaurant.Modify(CharInEachLine, spaceCountBetweenColumn);
-            ORestaurant.PrintLines();
-
-        }
-    }
-}
+// Create a new instance of the ReceiptPrinter class and ...
+ReceiptPrinter ORestaurant = new ReceiptPrinter(foodName, columnsBody, dateTime);
+ORestaurant.Modify(CharInEachLine, spaceCountBetweenColumn);
+ORestaurant.PrintLines();
