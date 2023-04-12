1f8645315b3767e62ceff305a1096b7e47c7ba38
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-04-12 06:25:47 +0600

    created a helper class of ReceiptPrinter that returns list of the bodies in columns, as a list.

--- POSPrinting/ReceiptPrinter.cs
+++ POSPrinting/ReceiptPrinter.cs
@@ -9,19 +9,24 @@ namespace POSPrinting
     internal class ReceiptPrinter
     {
         // Define class-level fields to store line size and comment data
-        private byte charInEachLine { get; set; }
-        private byte spaceCountBetweenColumn { get; set; }
-        public string[] columnsBody { get; set; }
+        private byte charInEachLine { get; set; } = 48;
+        public byte charInEachColumn { get; set; }
+        private byte spaceCountBetweenColumn { get; set; } = 1;
+        public List<List<string>> columnBodies { get; set; }
         private List<string> lines { get; set; }
         private int numberOfColumn { get; set; }
 
 
         // Constructor for the ReceiptPrinter class, which takes in the line size and comment data
         public ReceiptPrinter(params string[] columnsBody)
-        {
-            this.columnsBody = columnsBody;
+        { 
             this.numberOfColumn =  columnsBody.Count<string>();
+            charInEachColumn = (byte)(charInEachLine / numberOfColumn);
 
+            for (int i = 0; i < numberOfColumn; i++)
+            {
+                columnBodies.Add(StringModification.SplitCommentIntoLines(columnsBody[i], charInEachLine));
+            }
         }
         internal void Modify(byte charInEachLine, byte spaceCountBetweenColumn)
         {
